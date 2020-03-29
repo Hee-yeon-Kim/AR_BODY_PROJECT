d9d7a54e9dc0c143d4503d7943571958289d503a
heeyeona2 <heeyeona1@gmail.com>
2020-03-29 13:05:40 +0900

    s
    s

--- Assets/ShowOrigin.cs
+++ Assets/ShowOrigin.cs
@@ -12,15 +12,19 @@ public class ShowOrigin : MonoBehaviour
     public GameObject gizmo;
     private GameObject prefab1;
     public GameObject human;
+    public GameObject trace;
     private GameObject prefab2;
     private TrackableId floorID;
     private bool isFloor=false;
     private Plane floor;
-    private float eyedistance;
+    private float eyeheight;
     private float floorheight;
-    
+    private float distance;
+    private float timer;
+    private List<GameObject> footlist;
     public Text forTest1;
     // Start is called before the first frame update
+    string test13 = ""; string test12 = "";
     void Awake()
     {
         m_SessionOrigin = GetComponent<ARSessionOrigin>();
@@ -30,6 +34,7 @@ public class ShowOrigin : MonoBehaviour
     {
         
         Vector3 tempp= new Vector3();
+        footlist = new List<GameObject>();
         tempp = m_SessionOrigin.camera.transform.position;
 
         prefab1 = Instantiate(gizmo, tempp, Quaternion.identity);
@@ -38,6 +43,7 @@ public class ShowOrigin : MonoBehaviour
         prefab2 =Instantiate(human, tempp, Quaternion.identity);
         prefab2.transform.localScale = new Vector3(1.4f, 1.4f, 1.4f);
         prefab2.transform.SetParent(m_SessionOrigin .transform);
+        timer = 0;
         
       
         
@@ -45,32 +51,33 @@ public class ShowOrigin : MonoBehaviour
 
     // Update is called once per frame
     void Update()
-    { 
-        
+    {
+        timer += Time.deltaTime;
         //prefab1.transform.position = m_SessionOrigin.camera.transform.position;
         Vector3 direction = m_SessionOrigin.camera.transform.rotation * Vector3.forward;
         prefab1.transform.position = m_SessionOrigin.camera.transform.position + direction * 0.4f;
 
-         // prefab1.transform.Translate(new Vector3(0,0,0.5f),m_SessionOrigin.camera.transform);계속 움직임  멀리
-      //   prefab1.transform.rotation=m_SessionOrigin.camera.transform.rotation;
-      
-        Vector3 vemp2 = new Vector3(0,-2.3f,0);
-        Quaternion vempp = new Quaternion(0,0,0,0);
-        vempp.y = m_SessionOrigin.camera.transform.rotation.y;
-        vempp.w = m_SessionOrigin.camera.transform.rotation.w;
+        // prefab1.transform.Translate(new Vector3(0,0,0.5f),m_SessionOrigin.camera.transform);계속 움직임  멀리
+        //   prefab1.transform.rotation=m_SessionOrigin.camera.transform.rotation;
        
-        Vector3 vemp3 = new Vector3();
-         vemp3 = m_SessionOrigin.camera.transform.rotation * Vector3.back*1.3f;
+        //모델 포지션
+        Vector3 vemp2 = new Vector3();
+       
+        Vector3 vemp3 = new Vector3();//xz 설정
+         vemp3 = m_SessionOrigin.camera.transform.rotation * Vector3.back*2f;
         vemp3.y = 0;
 
-        /* Vector3 vemp = new Vector3();
-         vemp= m_SessionOrigin.camer a.transform.position+ m_SessionOrigin.camera.transform.rotation * Vector3.down * 2f+m_SessionOrigin.camera.transform.rotation* Vector3.back*0.4f;
-       vemp.x = m_SessionOrigin.camera.transform.position.x;
-        vemp.y = m_SessionOrigin.camera.transform.position.y - 2f;
-        vemp.z = m_SessionOrigin.camera.transform.position.z - 0.43f;*/
-     //   Quaternion n = new Quaternion(prefab2.transform.rotation.x, m_SessionOrigin.camera.transform.rotation.y, prefab2.transform.rotation.z, prefab2.transform.rotation.w);
-      //  prefab2.transform.rotation=  Quaternion.AngleAxis(90f, Vector3.up)*n;
-        prefab2.transform.position = m_SessionOrigin.camera.transform.position + vemp2+vemp3 ;
+        vemp2.x = m_SessionOrigin.camera.transform.position.x + vemp3.x;
+        vemp2.z = m_SessionOrigin.camera.transform.position.z + vemp3.z;
+        vemp2.y = floorheight * -1 - 0.7f;//키에 맞게 설정
+
+
+        prefab2.transform.position = vemp2;
+        //모델 rotation
+        Quaternion vempp = new Quaternion(0, 0, 0, 0);
+        vempp.y = m_SessionOrigin.camera.transform.rotation.y;
+        vempp.w = m_SessionOrigin.camera.transform.rotation.w;
+
         prefab2.transform.rotation = vempp;
         
 
@@ -92,7 +99,7 @@ public class ShowOrigin : MonoBehaviour
         string test106="다리포지션"+prefab2.transform.position.x.ToString()+" Ry: "+prefab1.transform.position.y.ToString()+" Rz: "+prefab1.transform.position.z.ToString();
         string test11="모델R"+prefab2.transform.rotation.x.ToString()+" Ry: "+prefab2.transform.rotation.y.ToString()+" Rz: "+prefab2.transform.rotation.z.ToString();
         string test9=" 평면갯수: "+m_ARPlaneManager.trackables.count.ToString();
-        string test13 = ""; string test12 = "";
+        
         if (isFloor == false)
         {
             foreach (ARPlane plane in m_ARPlaneManager.trackables)
@@ -101,11 +108,16 @@ public class ShowOrigin : MonoBehaviour
                 if (plane.center.y < -1.3f)
                 {
                     //prefab2.transform.position = new Vector3(prefab2.transform.position.x, plane.center.y, prefab2.transform.position.z);
+                    test12 = "찾다!! Local Plane x는: " + plane.transform.localPosition.x.ToString() + " y는: " + plane.transform.localPosition.y.ToString() + " z는: " + plane.transform.localPosition.z.ToString();
+
                     floorID = plane.trackableId;
                     floor = m_ARPlaneManager.GetPlane(floorID).infinitePlane;
+                    floorheight = floor.distance;
                     isFloor = true;
-                    test12 = "찾다!! Local Plane x는: " + plane.transform.localPosition.x.ToString() + " y는: " + plane.transform.localPosition.y.ToString() + " z는: " + plane.transform.localPosition.z.ToString();
-                    m_ARPlaneManager.SetTrackablesActive(false);
+                    m_ARPlaneManager.enabled = false;
+                     prefab2.transform.localScale = new Vector3(floorheight, floorheight, floorheight);
+                    test12 = "야찾다!! Local Plane x는: " + plane.transform.localPosition.x.ToString() + " y는: " + plane.transform.localPosition.y.ToString() + " z는: " + plane.transform.localPosition.z.ToString();
+                    
                     break;
                 }
 
@@ -115,12 +127,24 @@ public class ShowOrigin : MonoBehaviour
         else
         {
               
-            floorheight = floor.distance;
-            eyedistance=floor.GetDistanceToPoint(m_SessionOrigin.camera.transform.position);
-            test13 = "눈높이 " + eyedistance.ToString()+" 바닥높이: "+floorheight.ToString();
+           
+            eyeheight=floor.GetDistanceToPoint(m_SessionOrigin.camera.transform.position);
+            test13 = "눈높이 " + eyeheight.ToString()+" 바닥높이: "+floorheight.ToString();
 
         }
-        
+
+        if(timer>10)
+        {
+            Quaternion tmp = new Quaternion();
+            tmp = m_SessionOrigin.camera.transform.rotation;
+            tmp.x = 0; tmp.z = 0;
+            GameObject footprints=Instantiate(trace, new Vector3(m_SessionOrigin.camera.transform.position.x, -1*floorheight-1, m_SessionOrigin.camera.transform.position.z),  tmp);
+            footprints.transform.localScale = new Vector3(0.02f, 0.02f, 0.02f);
+            footprints.transform.SetParent(m_SessionOrigin.transform);
+            footlist.Add(footprints);
+            timer = 0;
+        }
+
         Quaternion temp=new Quaternion();
         Vector3 temp2= new Vector3();
         /*temp.x=m_SessionOrigin.camera.transform.rotation.x;
@@ -140,5 +164,12 @@ public class ShowOrigin : MonoBehaviour
 
   
 
-    }   
+    }
+    private void OnDisable()
+    {
+        foreach(GameObject i in footlist)
+        {
+            Destroy(i);
+        }
+    }
 }
